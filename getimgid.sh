92fddcf622d0224740c2d097544373bfd7a833c1
mahendra <maheshirala@gmail.com>
2019-04-01 05:18:42 +0000

    updated imagecleanup and getimgid script updated

--- getimgid.sh
+++ getimgid.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 imgID=$(sudo docker images | grep maheshirala/sample | awk '{print $3}')
-echo "\n" >> deployment.properties
-echo "imgid=$imgID" >> deployment.properties 
+
+if [ -z "$imgID" ]; then
+   echo "No Images to store."
+else
+   echo "\n" >> deployment.properties
+   echo "imgid=$imgID" >> deployment.properties
+fi
+
--- imagecleanup.sh
+++ imagecleanup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-imgID=$(docker images | grep maheshirala/sample | awk '{print $3}')
-echo "Clean up previous build image $imgID"
+
+imgID=$(cat deployment.properties | grep imgid | grep -v grep | awk -F '=' '{print $2}')
 
 if [ -z "$imgID" ]; then
    echo "No Images to delete."
 else
+   echo "Clean up previous build image $imgID"
    docker rmi $imgID
 fi
