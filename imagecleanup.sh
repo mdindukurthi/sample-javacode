c3120106834696ff210686e50a4e37235179c142
mahendra <maheshirala@gmail.com>
2019-04-01 11:02:18 +0000

    updated imageclenaup script

--- imagecleanup.sh
+++ imagecleanup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-imgID=$(cat deployment.properties | grep imgid | grep -v grep | awk -F '=' '{print $2}')
+#imgID=$(cat deployment.properties | grep imgid | grep -v grep | awk -F '=' '{print $2}')
+sourceid=$(cat deployment.properties | grep generatedid | grep -v grep | awk -F '=' '{print $2}')
 
-if [ -z "$imgID" ]; then
-   echo "No Images to delete."
+buildId=$(echo $sourceid | awk -F '-' '{print $3}')
+prvbuildId=$((buildId - 1))
+
+tag=$(echo $sourceid | cut -b 1-4)
+
+tag=$tag$prvbuildId
+
+echo "Image Tag Id: $tag"
+
+echo "Previous Build Id: $prvbuildId"
+
+imgId=$(sudo docker images maheshirala/sample:$tag | wc -l )
+echo $imgId
+if [ $imgId -gt 1 ]; then
+   echo "Clean up previous build image $imgID"
+   sudo docker rmi maheshirala/sample:$tag --force
+
 else
    echo "Clean up previous build image $imgID"
-   sudo docker rmi $imgID --force
+   sudo docker rmi maheshirala/sample:$tag --force
+   echo "No Images to delete."
 fi
